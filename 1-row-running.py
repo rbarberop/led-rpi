eb26d081159a832d2c2eb39634f14b429082f804
Roberto Barbero <rbarbero@gmail.com>
2023-12-01 22:04:54 +0000

    Fixing one row

--- 1-row-running.py
+++ 1-row-running.py
@@ -5,9 +5,9 @@ pixels = neopixel.NeoPixel(board.D18, 256, auto_write=False)
 
 while True:
   for i in range (0,31):
-    for j in range (0,7):
-      pixels[i+j]=(5,0,0)
+    for j in range (1,8):
+      pixels[i*j]=(5,0,0)
       pixels.show()
       sleep(0.02)
-      pixels[i+j]=(0,0,0)
+      pixels[i*j]=(0,0,0)
 
