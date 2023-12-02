cb5b01906f4d182abb4f8454ee656b1e918e5a6f
Roberto Barbero <rbarbero@gmail.com>
2023-12-02 12:57:54 +0000

    Testing the function

--- 1-row-running.py
+++ 1-row-running.py
@@ -7,8 +7,8 @@ pixels = neopixel.NeoPixel(board.D18, 256, auto_write=False)
 while True:
   for i in range (0,31):
     for j in range (1,8):
-      pixels[i*j]=(5,0,0)
+      pixels[tomatrix(i,j)]=(5,0,0)
       pixels.show()
       sleep(0.2)
-      pixels[i*j]=(0,0,0)
+      pixels[tomatrix(i,j)]=(0,0,0)
 
