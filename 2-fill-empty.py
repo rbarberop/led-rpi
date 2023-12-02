b3c171012248ba23de1c4a77da3b775fbdf706e7
Roberto Barbero <rbarbero@gmail.com>
2023-12-02 14:43:13 +0000

    Fix out of bounds color

--- 2-fill-empty.py
+++ 2-fill-empty.py
@@ -3,7 +3,7 @@ import board
 import neopixel
 from ledutils.tomatrix import tomatrix
 from ledutils.drawframe import drawframe
-pixels = neopixel.NeoPixel(board.D18, 256, brightness=0.05, auto_write=False)
+pixels = neopixel.NeoPixel(board.D18, 256, brightness=0.025, auto_write=False)
 from random import randint
 
 framecolor = (255,255,255)
@@ -11,7 +11,7 @@ framecolor = (255,255,255)
 while True:
   drawframe(pixels,framecolor)
   sleep(2)
-  fillcolor = (randint(0,256),randint(0,256),randint(0,256))
+  fillcolor = (randint(0,255),randint(0,255),randint(0,255))
   for i in range (1,31):
     for j in range (1,7):
       pixels[tomatrix(i,j)]=fillcolor
@@ -19,7 +19,7 @@ while True:
       sleep(0.02)
   drawframe(pixels,framecolor)
   sleep(2)
-  fillcolor = (randint(0,256),randint(0,256),randint(0,256))
+  fillcolor = (randint(0,255),randint(0,255),randint(0,255))
   for i in range (1,7):
     for j in range (1,31):
       pixels[tomatrix(j,i)]=fillcolor
