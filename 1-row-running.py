6c5fae92a6e312f8aeb6c71393204bce2908bf11
Roberto Barbero <rbarbero@gmail.com>
2023-12-02 13:24:26 +0000

    Matrix ok!

--- 1-row-running.py
+++ 1-row-running.py
@@ -1,14 +1,19 @@
 from time import sleep
 import board
 import neopixel
-from ledutils.tomatrix import tomatrix.tomatrix as tomatrix
+from ledutils.tomatrix import tomatrix
 pixels = neopixel.NeoPixel(board.D18, 256, auto_write=False)
 
 while True:
-  for i in range (0,31):
-    for j in range (1,8):
+  for i in range (0,32):
+    for j in range (0,8):
       pixels[tomatrix(i,j)]=(5,0,0)
       pixels.show()
-      sleep(0.2)
-      pixels[tomatrix(i,j)]=(0,0,0)
-
+      sleep(0.02)
+      pixels.fill((0,0,0))
+  for i in range (0,8):
+    for j in range (0,32):
+      pixels[tomatrix(j,i)]=(0,5,0)
+      pixels.show()
+      sleep(0.02)
+      pixels.fill((0,0,0))
