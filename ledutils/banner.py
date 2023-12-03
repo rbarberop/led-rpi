e9682b89b222306a544daeb53bf7f4bfe3648051
Roberto Barbero <rbarbero@gmail.com>
2023-12-03 14:54:12 +0000

    Fixing banner

--- ledutils/banner.py
+++ ledutils/banner.py
@@ -7,7 +7,7 @@ from time import sleep
 
 def banner(pixel, line, color):
   for a in range (0,32):
-      smallletter(pixel, line[0], a, (0,255,0))
+    smallletter(pixel, line[0], a, (0,255,0))
     sleep(0.2)
     pixel.clear()
 
