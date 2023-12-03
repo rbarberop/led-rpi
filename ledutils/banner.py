261188b490a8200dbfabd21d4ee4cb50812207de
Roberto Barbero <rbarbero@gmail.com>
2023-12-03 14:50:55 +0000

    Fixing banner

--- ledutils/banner.py
+++ ledutils/banner.py
@@ -1,20 +1,13 @@
 # Banner
 
 from .tomatrix import tomatrix
-from .smallfont import smallfont
+from .smallfont import smallletter
 from time import sleep
 
 
 def banner(pixel, line, color):
   for a in range (0,32):
-    for i in range(0,5):
-      for j in range(0,4):
-        for character in line:
-          if alphabet[character][i][j] == 0 :
-              pixel[tomatrix(j+a,i+2)]=(0,0,0)
-          else:
-              pixel[tomatrix(j+a,i+2)]=color
-    pixel.show()
+      smallletter(pixel, line[0], a, (0,255,0))
     sleep(0.2)
     pixel.clear()
 
