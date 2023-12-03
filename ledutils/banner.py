43748ab6c88eb7a65551de7042ee8024c45868f7
Roberto Barbero <rbarbero@gmail.com>
2023-12-03 15:24:15 +0000

    Fixing banner

--- ledutils/banner.py
+++ ledutils/banner.py
@@ -9,7 +9,7 @@ def banner(pixel, line, color):
   for col in range (32,(-5*len(line))-1,-1):
     letterpos = 1
     for letter in line:
-      smallletter(pixel, letter, (col*letterpos)+1, (0,255,0))
+      smallletter(pixel, letter, col+(5*letterpos)+1, (0,255,0))
       letterpos = letterpos + 1
     sleep(0.1)
     pixel.fill((0,0,0))
